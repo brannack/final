af089a95042afcc6beee66aa4521e674818e4eb7
Brannack <brannack@Brannacks-MacBook-Pro-2.local>
2017-03-11 00:19:30 -0600

    Final Project Initial Upload
    11 Mar 0019

---
+++ README.md
@@ -0,0 +1,23 @@
+# Buyflix Starter Project for KIEI-924
+
+Hosted live at https://buyflix-be19f.firebaseapp.com
+
+Brannack McLain
+Winter 2017
+
+
+## Getting Started
+
+Upon download or clone:
+
+```
+npm install
+```
+
+To start the development server:
+
+```
+npm start
+```
+
+For educational purposes only.
---
+++ src/index.js
@@ -0,0 +1,130 @@
+// React
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+// Google Maps
+var ReactGMaps = require('react-gmaps')
+var {Gmaps, Marker} = ReactGMaps
+
+// Movie data
+var movieData = require('./data/movies.json')
+var theatres = require('./data/theatres.json')
+
+// Components
+var Header = require('./components/Header')
+var MovieDetails = require('./components/MovieDetails')
+var MovieList = require('./components/MovieList')
+var NoCurrentMovie = require('./components/NoCurrentMovie')
+var TheatresMap = require('./components/TheatresMap')
+var SortBar = require('./components/SortBar')
+var SortBarItem = require('./components/SortBarItem')
+
+// Firebase configuration
+var Rebase = require('re-base')
+var base = Rebase.createClass({
+  apiKey: "AIzaSyDuwBWbgKimPlC4tS7dSO5fBZOIiK1k-Fc",
+  databaseURL: "https://buyflix-be19f.firebaseio.com/",
+})
+
+var App = React.createClass({
+  movieClicked: function(movie) {
+    this.setState({
+      currentMovie: movie
+    })
+  },
+  movieWatched: function(movie) {
+    var existingMovies = this.state.movies
+    var moviesWithWatchedMovieRemoved = existingMovies.filter(function(existingMovie) {
+      return existingMovie.id !== movie.id
+    })
+    this.setState({
+      movies: moviesWithWatchedMovieRemoved,
+      currentMovie: null
+    })
+  },
+  resetMovieListClicked: function() {
+    this.setState({
+      movies: movieData.sort(this.movieCompareByReleased)
+    })
+  },
+  getInitialState: function() {
+    return {
+      movies: movieData.sort(this.movieCompareByReleased),
+      currentMovie: null,
+      currentView: 'latest'
+    }
+  },
+  viewChanged: function(view) {
+        this.setState({
+          currentView: view
+        })
+        if (view === 'latest')
+        {
+          this.setState({
+          movies: movieData.sort(this.movieCompareByReleased)
+        }
+      )}
+        else if (view === 'alpha') {
+          this.setState({
+            movies: movieData.sort(this.movieCompareByTitle)
+          })
+        }
+    }
+,
+  renderMovieDetails: function() {
+    if (this.state.currentMovie == null) {
+      return <NoCurrentMovie resetMovieListClicked={this.resetMovieListClicked} />
+    } else {
+      return <MovieDetails movie={this.state.currentMovie}
+                           movieWatched={this.movieWatched} />
+    }
+  },
+  renderMainSection: function() {
+    if (this.state.currentView === 'map') {
+      return (
+        <TheatresMap />
+      )
+    } else {
+      return (
+        <div>
+          <MovieList movies={this.state.movies} movieClicked={this.movieClicked} />
+          {this.renderMovieDetails()}
+        </div>
+      )
+    }
+  },
+  movieCompareByTitle: function(movieA, movieB) {
+    if (movieA.title < movieB.title) {
+      return -1
+    } else if (movieA.title > movieB.title) {
+      return 1
+    } else {
+      return 0
+    }
+  },
+  movieCompareByReleased: function(movieA, movieB) {
+    if (movieA.released > movieB.released) {
+      return -1
+    } else if (movieA.released < movieB.released) {
+      return 1
+    } else {
+      return 0
+    }
+  },
+  componentDidMount: function() {
+    base.syncState('/movies', { context: this, state: 'movies', asArray: true })
+  },
+  render: function() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <SortBar currentView={this.state.currentView} movieCount={this.state.movies.length} viewChanged={this.viewChanged} />
+        <div className="main row">
+          {this.renderMainSection()}
+        </div>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById("app"))
